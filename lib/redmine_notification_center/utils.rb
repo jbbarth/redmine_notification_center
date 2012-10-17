5ce269ca496d5cc535ba2ceb3b8048013f62ff11
jbbarth <jeanbaptiste.barth@gmail.com>
2012-10-17 23:23:58 +0200

    Yay for stupid wrapper methods around redmine ones :/

---
+++ lib/redmine_notification_center/utils.rb
@@ -0,0 +1,13 @@
+module RedmineNotificationCenter
+  module Utils
+    extend self #so that method can be used directly on this namespace
+
+    def module_sends_notifications(mod)
+      pattern = mod.to_s.singularize
+      pattern.gsub!('issue_tracking', 'issue')
+      pattern.gsub!('board', 'message')
+      pattern = pattern+'_'
+      Setting.notified_events.detect{|n| n.starts_with?(pattern)}
+    end
+  end
+end
