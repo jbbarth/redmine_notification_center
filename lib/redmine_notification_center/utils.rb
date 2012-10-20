9f0c5808e01e9aff41e818f0f6283b43d9a870cf
jbbarth <jeanbaptiste.barth@gmail.com>
2012-10-20 23:29:21 +0200

    Rename RedmineNotificationCenter.module_sends_notifications to .module_sends_notifications?

--- lib/redmine_notification_center/utils.rb
+++ lib/redmine_notification_center/utils.rb
@@ -2,7 +2,7 @@ module RedmineNotificationCenter
   module Utils
     extend self #so that method can be used directly on this namespace
 
-    def module_sends_notifications(mod)
+    def module_sends_notifications?(mod)
       pattern = mod.to_s.singularize
       pattern.gsub!('issue_tracking', 'issue')
       pattern.gsub!('board', 'message')
