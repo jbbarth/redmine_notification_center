2a564cb73faea2319548476d1a546431304e1f7a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-10-24 15:18:56 +0200

    Move KNOW_EVENTS constant in RedmineNotificationCenter::Utils module

--- lib/redmine_notification_center/notification_event.rb
+++ lib/redmine_notification_center/notification_event.rb
@@ -1,11 +1,8 @@
 # Turns an A::R object into an event before notification
 module RedmineNotificationCenter
   class NotificationEvent < Struct.new(:type, :object)
-    KNOWN_EVENTS = %w(attachments_added document_added issue_added issue_edited
-                      message_posted news_added news_comment_added wiki_content_added wiki_content_updated)
-
     def initialize(type, object)
-      raise ArgumentError, "Unknown event type '#{type}'" unless KNOWN_EVENTS.include?(type.to_s)
+      raise ArgumentError, "Unknown event type '#{type}'" unless Utils::KNOWN_EVENTS.include?(type.to_s)
       super
     end
 
--- lib/redmine_notification_center/utils.rb
+++ lib/redmine_notification_center/utils.rb
@@ -2,6 +2,9 @@ module RedmineNotificationCenter
   module Utils
     extend self #so that method can be used directly on this namespace
 
+    KNOWN_EVENTS = %w(attachments_added document_added issue_added issue_edited
+                      message_posted news_added news_comment_added wiki_content_added wiki_content_updated)
+
     def module_sends_notifications?(mod)
       pattern = mod.to_s.singularize
       pattern.gsub!('issue_tracking', 'issue')
