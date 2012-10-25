5192dfca935d6f42277beb0de80064ba6f711286
jbbarth <jeanbaptiste.barth@gmail.com>
2012-10-25 19:58:12 +0200

    Refactor NotificationPolicy ; still a lot to do but this is a first step

--- lib/redmine_notification_center/notification_policy.rb
+++ lib/redmine_notification_center/notification_policy.rb
@@ -5,24 +5,106 @@ module RedmineNotificationCenter
     end
 
     def should_be_notified_for?(notification_event)
-      event_type, object = notification_event.type, notification_event.object
-      notif = @user.notification_preferences
-      return false if notif[:none_at_all] == '1'
-      #TODO: exceptions! + don't use issue exceptions if issue author, issue assignee or watcher
-      return true if notif[:all_events] == '1'
+      if user_doesnt_want_any_notification
+        false
+      elsif user_wants_all_notifications && exceptions_dont_apply
+        true
+      else
+        event_type, object = notification_event.type, notification_event.object
+        case Utils.module_from_event(event_type)
+        when :files
+          file_notification_for(event_type, object)
+        when :documents
+          document_notification_for(event_type, object)
+        when :issue_tracking
+          issue_notification_for(event_type, object)
+        when :boards
+          board_notification_for(event_type, object)
+        when :news
+          news_notification_for(event_type, object)
+        when :wiki
+          wiki_notification_for(event_type, object)
+        else
+          #TODO: we shouldn't reach this line ; replace it with an exception when every possible event_type is handled
+          raise "You should never reach this line"
+        end
+      end
+    end
+
+    def pref
+      @pref ||= @user.notification_preferences
+    end
+
+    private
+    def user_doesnt_want_any_notification
+      pref[:none_at_all] == '1'
+    end
+
+    def user_wants_all_notifications
+      pref[:all_events] == '1'
+    end
+
+    #TODO: exceptions! + don't use issue exceptions if issue author, issue assignee or watcher
+    def exceptions_dont_apply
+      true #no exception for now!
+    end
+
+    # 
+    # FILES
+    #
+    def file_notification_for(event_type, object)
+      #TODO: be clever!
+      true
+    end
+
+    #
+    # DOCUMENTS
+    #
+    def document_notification_for(event_type, object)
+      #TODO: be clever!
+      true
+    end
+
+    #
+    # ISSUES
+    #
+    def issue_notification_for(event_type, object)
       #TODO: return true or false immediately if module identified && by_module.<module> == 'all' or 'none'
-      #custom modules
-      if [:issue_added, :issue_edited].include?(event_type) && notif[:by_module][:issue_tracking] == 'custom'
+      if pref[:by_module][:issue_tracking] == 'custom'
         #TODO: handle watcher case
         if object.author == @user || object.author_was == @user
-          return notif[:by_module][:issue_tracking_custom][:if_author] == '1'
+          return pref[:by_module][:issue_tracking_custom][:if_author] == '1'
         end
         if object.assignee == @user || object.assignee_was == @user
-          return notif[:by_module][:issue_tracking_custom][:if_assignee] == '1'
+          return pref[:by_module][:issue_tracking_custom][:if_assignee] == '1'
         end
-        return notif[:by_module][:issue_tracking_custom][:others] == '1'
+        return pref[:by_module][:issue_tracking_custom][:others] == '1'
       end
       #TODO: we shouldn't reach this line ; replace it with an exception when every possible event_type is handled
+      true
+    end
+
+    #
+    # BOARDS
+    #
+    def board_notification_for(event_type, object)
+      #TODO: be clever!
+      true
+    end
+
+    #
+    # NEWS
+    # 
+    def news_notification_for(event_type, object)
+      #TODO: be clever!
+      true
+    end
+
+    #
+    # WIKI
+    #
+    def wiki_notification_for(event_type, object)
+      #TODO: be clever!
       true
     end
   end
